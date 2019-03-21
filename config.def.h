08771f52446a1a3f973780919879f0638ae70656
Paper Benni <paperbenni@gmail.com>
2019-03-21 17:59:51 +0100

    fuzzy search

--- config.def.h
+++ config.def.h
@@ -2,6 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Monaco:size=12"
