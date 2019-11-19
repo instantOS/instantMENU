6ff4862df1878cf369fb3ef5cbd1bb5368047b46
paperbenni <paperbenni@gmail.com>
2019-11-19 18:44:17 +0100

    theme chooser works now

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-rm config.h
-make clean
+make clean &>/dev/null
+
 THEME=themes/${1:-dracula}.theme
-grep -q 'font' <$THEME || exit 1
+echo "$THEME"
+grep -q 'size' <$THEME || { echo "theme not valid" && exit 1; }
 
 replacetheme() {
     themecolor=$(grep "$1" <$THEME)
-    sed -i 's/.*'"$1"'.*/'"$themecolor"'./' config.def.h
+    sed -i 's/.*'"$1"'.*/'"$themecolor"'/' config.def.h
 }
 
 replacetheme SchemeNorm
@@ -15,5 +16,7 @@ replacetheme SchemeSel
 replacetheme SchemeOut
 replacetheme "size="
 
-make
-sudo make install
+if [ -z "$2" ]; then
+    make
+    sudo make install
+fi
