fece5acb0ac757705fcba45ac7992274b3fc3320
paperbenni <paperbenni@gmail.com>
2020-01-26 13:56:32 +0100

    split build and theme.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,8 @@
 #!/usr/bin/env bash
 
+./theme.sh "$1"
+
 make clean &>/dev/null
-
-THEME=themes/${1:-dracula}.theme
-echo "$THEME"
-grep -q 'size' <$THEME || { echo "theme not valid" && exit 1; }
-
-replacetheme() {
-    themecolor=$(grep "$1" <$THEME)
-    sed -i 's/.*'"$1"'.*/'"$themecolor"'/' config.def.h
-}
-
-replacetheme SchemeNorm
-replacetheme SchemeSel
-replacetheme SchemeOut
-replacetheme "size="
 
 if [ -z "$2" ]; then
     make
---
+++ theme.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+THEME=themes/${1:-dracula}.theme
+echo "$THEME"
+grep -q 'size' <$THEME || { echo "theme not valid" && exit 1; }
+
+replacetheme() {
+    themecolor=$(grep "$1" <$THEME)
+    sed -i 's/.*'"$1"'.*/'"$themecolor"'/' config.def.h
+}
+
+replacetheme SchemeNorm
+replacetheme SchemeSel
+replacetheme SchemeOut
+replacetheme "size="
