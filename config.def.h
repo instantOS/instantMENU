4a4bdda71236d30d4e2f73eaf14179017a87b625
paperbenni <paperbenni@gmail.com>
2020-07-10 15:34:52 +0200

    fix emoji font

--- config.def.h
+++ config.def.h
@@ -12,7 +12,8 @@ static unsigned int lineheight = 0;         /* -h option; minimum height of a me
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Cantarell-Regular:size=12"
+	"Cantarell-Regular:size=12", 
+	"JoyPixels:pixelsize=10:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][3] = {
