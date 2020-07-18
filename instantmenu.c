85c881698fcf3d2e405adcdff567f77a9eb13385
paperbenni <paperbenni@gmail.com>
2020-07-18 10:41:53 +0200

    fix mouse button problem

--- instantmenu.c
+++ instantmenu.c
@@ -947,14 +947,74 @@ buttonpress(XEvent *e)
 	/* left-click on input: clear input,
 	 * NOTE: if there is no left-arrow the space for < is reserved so
 	 *       add that to the input width */
-	if (ev->button == Button1 &&
-	   ((lines <= 0 && ev->x >= 0 && ev->x <= x + w +
-	   ((!prev || !curr->left) ? TEXTW("<") : 0)) ||
-	   (lines > 0 && ev->y >= y && ev->y <= y + h))) {
-		insert(NULL, -cursor);
-		drawmenu();
-		return;
+	if (ev->button == Button1) {
+		if ((lines <= 0 && ev->x >= 0 && ev->x <= x + w +
+		((!prev || !curr->left) ? TEXTW("<") : 0)) ||
+		(lines > 0 && ev->y >= y && ev->y <= y + h)) {
+			insert(NULL, -cursor);
+			drawmenu();
+			return;
+		} else {
+			if (lines > 0) {
+				/* vertical list: (ctrl)left-click on item */
+				w = mw - x;
+				item = sel;
+				if (sel && sel->text[0] == '>')
+					return;
+				animatesel();
+				puts(item->text);
+				if (!(ev->state & ControlMask))
+					exit(0);
+				sel = item;
+				if (sel) {
+					sel->out = 1;
+					drawmenu();
+				}
+				return;
+			} else if (matches) {
+				/* left-click on left arrow */
+				x += inputw;
+				w = TEXTW("<");
+				if (prev && curr->left) {
+					if (ev->x >= x && ev->x <= x + w) {
+						sel = curr = prev;
+						calcoffsets();
+						drawmenu();
+						return;
+					}
+				}
+				/* horizontal list: (ctrl)left-click on item */
+				for (item = curr; item != next; item = item->right) {
+					x += w;
+					w = MIN(TEXTW(item->text), mw - x - TEXTW(">"));
+					if (ev->x >= x && ev->x <= x + w) {
+						if (sel && item->text[0] == '>')
+							break;
+						animatesel();
+						puts(item->text);
+						if (!(ev->state & ControlMask))
+							exit(0);
+						sel = item;
+						if (sel) {
+							sel->out = 1;
+							drawmenu();
+						}
+						return;
+					}
+				}
+				/* left-click on right arrow */
+				w = TEXTW(">");
+				x = mw - w;
+				if (next && ev->x >= x && ev->x <= x + w) {
+					sel = curr = next;
+					calcoffsets();
+					drawmenu();
+					return;
+				}
+			}
+		}
 	}
+
 	/* middle-mouse click: paste selection */
 	if (ev->button == Button2) {
 		XConvertSelection(dpy, (ev->state & ShiftMask) ? clip : XA_PRIMARY,
@@ -975,67 +1035,6 @@ buttonpress(XEvent *e)
 		calcoffsets();
 		drawmenu();
 		return;
-	}
-	if (ev->button != Button1)
-		return;
-	if (ev->state & ~ControlMask & ~Mod1Mask)
-		return;
-	if (lines > 0) {
-		/* vertical list: (ctrl)left-click on item */
-		w = mw - x;
-		item = sel;
-		if (sel && sel->text[0] == '>')
-			return;
-		animatesel();
-		puts(item->text);
-		if (!(ev->state & ControlMask))
-			exit(0);
-		sel = item;
-		if (sel) {
-			sel->out = 1;
-			drawmenu();
-		}
-		return;
-	} else if (matches) {
-		/* left-click on left arrow */
-		x += inputw;
-		w = TEXTW("<");
-		if (prev && curr->left) {
-			if (ev->x >= x && ev->x <= x + w) {
-				sel = curr = prev;
-				calcoffsets();
-				drawmenu();
-				return;
-			}
-		}
-		/* horizontal list: (ctrl)left-click on item */
-		for (item = curr; item != next; item = item->right) {
-			x += w;
-			w = MIN(TEXTW(item->text), mw - x - TEXTW(">"));
-			if (ev->x >= x && ev->x <= x + w) {
-				if (sel && item->text[0] == '>')
-					break;
-				animatesel();
-				puts(item->text);
-				if (!(ev->state & ControlMask))
-					exit(0);
-				sel = item;
-				if (sel) {
-					sel->out = 1;
-					drawmenu();
-				}
-				return;
-			}
-		}
-		/* left-click on right arrow */
-		w = TEXTW(">");
-		x = mw - w;
-		if (next && ev->x >= x && ev->x <= x + w) {
-			sel = curr = next;
-			calcoffsets();
-			drawmenu();
-			return;
-		}
 	}
 }
 
