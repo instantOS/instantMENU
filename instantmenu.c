23e93c965c61da2048b989269e40d39e3355b90e
paperbenni <paperbenni@gmail.com>
2023-09-14 15:24:44 +0200

    merge upstream until 0fe460dbd469a1d5b6a7140d0e1801935e4a923b

--- instantmenu.c
+++ instantmenu.c
@@ -1768,6 +1768,7 @@ setup(void)
 
 	XMapRaised(dpy, win);
 	if (embed) {
+        XReparentWindow(dpy, win, parentwin, x, y);
 		XSelectInput(dpy, parentwin, FocusChangeMask | SubstructureNotifyMask);
 		if (XQueryTree(dpy, parentwin, &dw, &w, &dws, &du) && dws) {
 			for (i = 0; i < du && dws[i] != win; ++i)
