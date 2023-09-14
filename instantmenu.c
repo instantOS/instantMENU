35215879dce14675c6819e07f25b9740b8b9c205
paperbenni <paperbenni@gmail.com>
2023-09-14 15:17:57 +0200

    merge upstream until ba1a347dcaba055f824161007dfee60db3ea785b

--- instantmenu.c
+++ instantmenu.c
@@ -1451,7 +1451,7 @@ static void
 readstdin(void)
 {
 	char *line = NULL;
-	size_t i, junk, size = 0;
+	size_t i, junk, itemsiz = 0;
 	ssize_t len;
 
  	if(passwd || inputonly){
@@ -1461,9 +1461,11 @@ readstdin(void)
 
 	/* read each line from stdin and add it to the item list */
 	for (i = 0; (len = getline(&line, &junk, stdin)) != -1; i++) {
-		if (i + 1 >= size / sizeof *items)
-			if (!(items = realloc(items, (size += BUFSIZ))))
-				die("cannot realloc %zu bytes:", size);
+        if (i + 1 >= itemsiz) {
+            itemsiz += 256;
+            if (!(items = realloc(items, itemsiz * sizeof(*items))))
+                die("cannot realloc %zu bytes:", itemsiz * sizeof(*items));
+        }
 		if (line[len - 1] == '\n' ||line[len - 1] == '\t' )
 			line[len - 1] = '\0';
         items[i].stext = line;
